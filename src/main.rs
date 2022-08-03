6a99a1a0368a3e8353c7efcfc8354d7a2b293ad8
Valentin Fries <valentin_fries@connect-tech.sncf>
2022-08-03 21:59:24 +0200

    Render an entity on screen

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,61 @@
-use rltk::{GameState, Rltk};
+#[forbid(warnings)]
 
-struct State {}
+use rltk::{FontCharType, GameState, Rltk, RltkBuilder, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: FontCharType,
+    foreground: RGB,
+    background: RGB,
+}
+
+struct State {
+    world: World,
+}
 
 impl GameState for State {
-    
+
     fn tick(&mut self, context: &mut Rltk) {
         context.cls();
-        context.print(1, 1, "Hello World!");
+
+        let positions = self.world.read_storage::<Position>();
+        let renderables = self.world.read_storage::<Renderable>();
+
+        for (position, renderable) in (&positions, &renderables).join() {
+            context.set(position.x, position.y, renderable.foreground, renderable.background, renderable.glyph);
+        }
     }
 }
 
 fn main() -> rltk::BError {
-    let context = rltk::RltkBuilder::simple80x50()
+    let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let state = State {};
+    let mut state = State {
+        world: World::new(),
+    };
+
+    state.world.register::<Position>();
+    state.world.register::<Renderable>();
+
+    state
+        .world
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            foreground: RGB::named(rltk::YELLOW),
+            background: RGB::named(rltk::BLACK),
+        })
+        .build();
+
     rltk::main_loop(context, state)
 }
