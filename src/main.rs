01d97ca05c35737080513275087c09645ec97c47
Valentin Fries <valentin_fries@connect-tech.sncf>
2022-08-03 22:23:24 +0200

    Dummy ia: walk left

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #[forbid(warnings)]
-
 use rltk::{FontCharType, GameState, Rltk, RltkBuilder, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
@@ -17,20 +16,52 @@ struct Renderable {
     background: RGB,
 }
 
+#[derive(Component)]
+struct LeftWalker {}
+
+struct WalkLeft {}
+
+impl<'a> System<'a> for WalkLeft {
+    type SystemData = (ReadStorage<'a, LeftWalker>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (left_walkers, mut positions): Self::SystemData) {
+        for (_left_walker, position) in (&left_walkers, &mut positions).join() {
+            position.x -= 1;
+            if position.x < 0 {
+                position.x = 79;
+            }
+        }
+    }
+}
+
 struct State {
     world: World,
 }
 
-impl GameState for State {
+impl State {
+    fn run_systems(&mut self) {
+        let mut walk_left = WalkLeft {};
+        walk_left.run_now(&self.world);
+        self.world.maintain();
+    }
+}
 
+impl GameState for State {
     fn tick(&mut self, context: &mut Rltk) {
         context.cls();
+        self.run_systems();
 
         let positions = self.world.read_storage::<Position>();
         let renderables = self.world.read_storage::<Renderable>();
 
         for (position, renderable) in (&positions, &renderables).join() {
-            context.set(position.x, position.y, renderable.foreground, renderable.background, renderable.glyph);
+            context.set(
+                position.x,
+                position.y,
+                renderable.foreground,
+                renderable.background,
+                renderable.glyph,
+            );
         }
     }
 }
@@ -45,6 +76,7 @@ fn main() -> rltk::BError {
 
     state.world.register::<Position>();
     state.world.register::<Renderable>();
+    state.world.register::<LeftWalker>();
 
     state
         .world
@@ -56,6 +88,23 @@ fn main() -> rltk::BError {
             background: RGB::named(rltk::BLACK),
         })
         .build();
+
+    for i in 0..10 {
+        state
+            .world
+            .create_entity()
+            .with(Position {
+                x: i * 7,
+                y: i + 20,
+            })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                foreground: RGB::named(rltk::RED),
+                background: RGB::named(rltk::BLACK),
+            })
+            .with(LeftWalker {})
+            .build();
+    }
 
     rltk::main_loop(context, state)
 }
