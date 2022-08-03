d1973ecffe4f1eae2861527d2227491c25f10bf2
Valentin Fries <valentin_fries@connect-tech.sncf>
2022-08-03 23:55:57 +0200

    Added a map with floor and walls

--- src/main.rs
+++ src/main.rs
@@ -44,10 +44,14 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, world: &mut World) {
     let players = world.read_storage::<Player>();
     let mut positions = world.write_storage::<Position>();
+    let map = world.fetch::<Vec<TileType>>();
 
     for (_player, position) in (&players, &mut positions).join() {
-        position.x = min(WIDTH - 1, max(0, position.x + delta_x));
-        position.y = min(HEIGHT - 1, max(0, position.y + delta_y));
+        let destination_idx = xy_idx(position.x + delta_x, position.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            position.x = min(WIDTH - 1, max(0, position.x + delta_x));
+            position.y = min(HEIGHT - 1, max(0, position.y + delta_y));
+        }
     }
 }
 
@@ -61,6 +65,80 @@ fn player_input(state: &mut State, context: &mut Rltk) {
             VirtualKeyCode::Down => try_move_player(0, 1, &mut state.world),
             _ => {}
         },
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * WIDTH as usize) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; (WIDTH * HEIGHT) as usize];
+
+    // Make the boundaries walls
+    for x in 0..WIDTH {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, HEIGHT - 1)] = TileType::Wall;
+    }
+    for y in 0..HEIGHT {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(WIDTH - 1, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, WIDTH - 1);
+        let y = rng.roll_dice(1, HEIGHT - 1);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], context: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                context.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                context.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > WIDTH - 1 {
+            x = 0;
+            y += 1;
+        }
     }
 }
 
@@ -81,6 +159,9 @@ impl GameState for State {
 
         player_input(self, context);
         self.run_systems();
+
+        let map = self.world.fetch::<Vec<TileType>>();
+        draw_map(&map, context);
 
         let positions = self.world.read_storage::<Position>();
         let renderables = self.world.read_storage::<Renderable>();
@@ -105,6 +186,8 @@ fn main() -> rltk::BError {
         world: World::new(),
     };
 
+    state.world.insert(new_map());
+
     state.world.register::<Player>();
     state.world.register::<Position>();
     state.world.register::<Renderable>();
@@ -122,7 +205,7 @@ fn main() -> rltk::BError {
         })
         .build();
 
-    for i in 0..10 {
+    /*for i in 0..10 {
         state
             .world
             .create_entity()
@@ -137,7 +220,7 @@ fn main() -> rltk::BError {
             })
             .with(LeftWalker {})
             .build();
-    }
+    }*/
 
     rltk::main_loop(context, state)
 }
