c42f170928d77590a4601afbe7d78f2063751783
Valentin Fries <valentin_fries@connect-tech.sncf>
2022-08-03 20:57:40 +0200

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk};
+
+struct State {}
+
+impl GameState for State {
+    
+    fn tick(&mut self, context: &mut Rltk) {
+        context.cls();
+        context.print(1, 1, "Hello World!");
+    }
+}
+
+fn main() -> rltk::BError {
+    let context = rltk::RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let state = State {};
+    rltk::main_loop(context, state)
 }
